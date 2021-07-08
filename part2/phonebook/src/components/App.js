8910931b2d274ce0d0869ac642acc4fc726ea4d4
jesus cano <jesuscano@yahoo.es>
2021-07-08 15:33:29 +0200

    phonebook step 3 and 4

--- part2/phonebook/src/components/App.js
+++ part2/phonebook/src/components/App.js
@@ -1,15 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', phone: "004495" }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newPhone, setNewPhone ] = useState("")
+  const [ filter, setNewFilter ] = useState("")
+  const [ found, setNewFound ] = useState({})
+  useEffect(() => { 
+    const check=persons.map(person=> person.name.trim().toLowerCase()).indexOf(filter)
+    console.log(filter);
+    console.log(persons.map(person=> person.name.trim().toLowerCase()));
+    console.log(check);
+    if(check>=0){
+        console.log("si está");
+        setNewFound(persons[check])
+    }
+  }, [filter,persons])
+
   const addPerson = (event)=>{
       event.preventDefault();
       const arrayPosition=persons.map(person=> person.name).indexOf(newName);
       if(arrayPosition===-1){   
-        let newAdd= {name:newName};
+        let newAdd= {name:newName, phone: newPhone};
         setPersons(persons.concat(newAdd));
       }
       else{
@@ -18,16 +32,27 @@ const App = () => {
       
       
   }
-  const handleInput = (event)=>{
+  const handleName = (event)=>{
       setNewName(event.target.value);
+  }
+  const handlePhone = (event)=>{
+      setNewPhone(event.target.value);
+  }
+
+  const filterit = (event)=>{
+     setNewFilter((event.target.value).toLowerCase());
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <p>Filter shown with: <input onChange={filterit}></input></p>
+      <p>Found: {found.name} {found.phone}</p>
+      <h3>Add new</h3>
       <form onSubmit={addPerson}>
         <div>
-          name: <input onChange={handleInput} value={newName}/>
+          name: <input onChange={handleName} value={newName}/>
+          phone: <input onChange={handlePhone} value={newPhone}/> 
         </div>
         <div>
           <button type="submit">add</button>
@@ -35,7 +60,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
     
-      <div> {persons.map(person=><p key={person.name}> {person.name}</p>)}</div>
+      <div> {persons.map(person=><p key={person.name}> {person.name} {person.phone}</p>)}</div>
       
     </div>
   )
---
+++ part2/phonebook/src/components/Filter.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Filter=()=>{
+
+    const filterFunc=()=>{
+        
+    }
+    return(<div>
+        <input onChange={filterFunc}></input>
+    </div>)
+}
+
+export default Filter;
